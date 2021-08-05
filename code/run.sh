ba074ab415e02c700cd033660641dbf30f8d511e
jesseorg <jesse@jesse>
2021-08-05 14:06:46 -0700

    Added interpolation, see readme for notes.

--- code/run.sh
+++ code/run.sh
@@ -1,7 +1,7 @@
 echo "This script will take the words you enter on command line, and generate birds."
 echo "It requires the 2106style Venv"
 
-source code/2106style/bin/activate
+source /org/dp/attngan/Style-AttnGAN/code/2106style/bin/activate
 
 echo $1  > ../data/birds/20210614.txt
 python main.py --cfg cfg/eval_bird_style.yml --text_encoder_type rnn --gpu 0 --frames 12
